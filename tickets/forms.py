d3ae28c69a926161091c5bc50c34c0253b19c499
SnayderCJ <jcedenoj5@unemi.edu.ec>
2024-06-30 23:07:03 -0500

    'Update'

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from .models import Ticket
+
+class TicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['nombre', 'email', 'descripcion', 'prioridad']
+
+    def clean_prioridad(self):
+        prioridad = self.cleaned_data['prioridad']
+        if not 1 <= prioridad <= 5:
+            raise forms.ValidationError('La prioridad debe estar entre 1 y 5')
+        return prioridad
